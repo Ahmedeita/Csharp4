1108769aaeb01481b063a478cf0967a98b427132
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-20 23:35:20 +0200

    8- . Write a program that prints an identity matrix using for loop, in other words takes a value n from the user and shows the identity table of size n * n.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -103,6 +103,29 @@ namespace Assignment
             //}
             //Console.Write("The Binary of 25 is " + sb);
             #endregion
+
+            #region Q8
+            //8- . Write a program that prints an identity matrix using for loop,
+            //in other words takes a value n from the user and shows the identity table of size n * n.
+
+            //Console.Write("Enter the size of the identity matrix : ");
+            //int n = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < n; i++)
+            //{
+            //    for (int j = 0; j < n; j++)
+            //    {
+            //        if (i == j)
+            //           Console.Write("1 ");
+                    
+            //        else
+            //          Console.Write("0 ");
+                    
+            //    }
+            //    Console.WriteLine();
+            //}
+
+            #endregion
         }
     }
 }

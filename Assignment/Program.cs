906c03ee56680f80f0749484dd83cd2960397bc2
Ahmedeita <ahmedeita.dev@gmail.com>
2024-12-20 22:31:13 +0200

    1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q1
+            //1- Write a program that allows the user to insert an integer then print all numbers between 1 to that number.
+            //Console.Write("Entere Integer Number : ");
+            //int Number;
+            //int.TryParse(Console.ReadLine(), out Number);
+            //for (int i = 1; i <= Number; i++)
+            //    Console.Write($" {i},");
+            #endregion
+
+
         }
     }
 }
